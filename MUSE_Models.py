21245dd9f72270a59c83624e6639ea1286ee73de
Thomas Spriggs <tspriggs@outlook.com>
2018-09-04 10:48:03 +0100

    fitting Hb added in simple 1D method

--- MUSE_Models.py
+++ MUSE_Models.py
@@ -43,6 +43,53 @@ def MUSE_3D_OIII(params, l, x_2D, y_2D, data, model_2D):
     elif model_2D == "Gauss_2":
         return model_spectra, [np.max(A_OIII_xy), F_OIII_xy, A_OIII_xy, Gauss_1, Gauss_2]
 
+# Multi wavelength analysis model
+def MUSE_3D_OIII_multi_wave(params, l, x_2D, y_2D, data, n_lines):
+    # n_lines is a dictionary of wavelengths with offsets, read from this and make models with offsets
+    Amp_2D_OIII = params['Amp_2D']
+    x_0 = params['x_0']
+    y_0 = params['y_0']
+    M_FWHM = params["M_FWHM"]
+    beta = params["beta"]
+    mean_OIII = params["mean"]
+    Gauss_bkg = params["Gauss_bkg"]
+    Gauss_grad = params["Gauss_grad"]
+
+    #Moffat model
+    gamma = M_FWHM / (2. * np.sqrt(2.**(1./beta) - 1.))
+    rr_gg = ((x_2D - x_0)**2. + (y_2D - y_0)**2) / gamma**2.
+    F_OIII_xy = Amp_2D_OIII * ((1 + rr_gg)**(-beta))
+
+    # 1D Gaussian standard deviation from FWHM
+    Gauss_std = 2.81 / 2.35482
+
+    # Convert flux to amplitude
+    A_OIII_xy = ((F_OIII_xy) / (np.sqrt(2*np.pi) * Gauss_std))
+
+    #Construct OIII model
+    OIII_model_spectra = [(Gauss_bkg + (Gauss_grad * l) + Amp * np.exp(- 0.5 * (l - mean_OIII)** 2 / Gauss_std**2.) +
+             (Amp/3.) * np.exp(- 0.5 * (l - (mean_OIII - 47.9399))** 2 / Gauss_std**2.)) for Amp in A_OIII_xy]
+    
+    def generate_model(x, y, moffat_amp, FWHM, beta, Gauss_FWHM, Gauss_bkg, Gauss_grad, mean):
+        gamma = FWHM / (2. * np.sqrt(2.**(1./beta) - 1.))
+        rr_gg = ((x_2D - x)**2. + (y_2D - y)**2.) / gamma**2.
+        F_OIII_xy = moffat_amp * (1. + rr_gg)**(-beta)
+
+        comb_FWHM = np.sqrt(2.81**2. + Gauss_FWHM**2.)
+        Gauss_std = comb_FWHM / 2.35482
+
+        A_OIII_xy = ((F_OIII_xy) / (np.sqrt(2*np.pi) * Gauss_std))
+
+        model_spectra = [(Gauss_bkg + (Gauss_grad * l) + np.abs(Amp) * np.exp(- 0.5 * (l - mean)** 2. / Gauss_std**2.) +
+             (np.abs(Amp)/3.) * np.exp(- 0.5 * (l - (mean - 47.9399))** 2. / Gauss_std**2.)) for Amp in A_OIII_xy]
+
+        return model_spectra
+    
+    for k in np.arange(0, len(data)):
+        list_of_models["model_{:03d}".format(k)] = generate_model(x_0, y_0, PNe_params["moffat_amp_{:03d}".format(k)], FWHM, beta, Gauss_FWHM, gauss_grad, gauss_bkg, PNe_params["mean_{:03d}".format(k)])
+    
+    return model_spectra, [np.max(A_OIII_xy), F_OIII_xy, A_OIII_xy]
+# end dev
 
 def MUSE_3D_residual(params, l, x_2D, y_2D, data, error, model_2D, PNe_number, list_to_append_data):
     model = MUSE_3D_OIII(params, l, x_2D, y_2D, data, model_2D )
@@ -62,9 +109,6 @@ def Gaussian_1D_res(params, x, data, error, spec_num):
     Gauss_std = FWHM / 2.35482
     model = ((Gauss_bkg + Gauss_grad * x) + Amp * np.exp(- 0.5 * (x - mean)** 2 / Gauss_std**2.) +
              (Amp/3.) * np.exp(- 0.5 * (x - (mean - 47.9399))** 2 / Gauss_std**2.))
-
-    list_of_rN[spec_num] = np.std(data - model)
-    list_of_residuals[spec_num] = data - model
 
     return (data - model) / error
 
