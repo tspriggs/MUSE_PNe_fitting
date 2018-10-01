895278d151f547052bbb8d11f59bba28f228eb4e
Thomas Spriggs <tspriggs@outlook.com>
2018-10-01 13:34:04 +0100

    sync

--- multi_fit_test.py
+++ multi_fit_test.py
@@ -15,7 +15,7 @@ hdulist = fits.open("FCC277_data/FCC277_emission_cube.fits")
 #hdulist_stellar = fits.open("FCC277_data/FCC277_Gandalf_stellar_cube.fits")
 raw_data = hdulist[0].data
 #raw_data_stellar = hdulist_stellar[0].data
-hdr = hdulist[0].header
+#hdr = hdulist[0].header
 
 #full_wavelength = np.exp(lnl[0].data)
 #np.save("exported_data/FCC277/wavelength", full_wavelength)
@@ -49,9 +49,9 @@ print("starting fit")
 
 def Gaussian_1D_res_full(params, l, data, error, spec_num):
     Amp_list = [params["Amp_OIII_5007"], params["Amp_OIII_4959"], params["Amp_Hb"],
-                params["Amp_Ha"], params["Amp_NII_1"], params["Amp_NII_2"]]
+                params["Amp_Ha"]]#, params["Amp_NII_1"], params["Amp_NII_2"]]
     wave_list = [params["wave_OIII_5007"], params["wave_OIII_4959"], params["wave_Hb"],
-                params["wave_Ha"], params["wave_NII_1"], params["wave_NII_2"]]
+                params["wave_Ha"]]#, params["wave_NII_1"], params["wave_NII_2"]]
     FWHM = params["FWHM"]
     Gauss_bkg = params["Gauss_bkg"]
     Gauss_grad = params["Gauss_grad"]
@@ -61,24 +61,24 @@ def Gaussian_1D_res_full(params, l, data, error, spec_num):
 
 
     list_of_rN[spec_num] = np.std(data - model)
-    list_of_residuals[spec_num] = data - model
+    #list_of_residuals[spec_num] = data - model
 
     return (data - model) / error
 
 params = Parameters()
-params.add("Amp_OIII_5007",value=70., min=0.001)
-params.add("Amp_OIII_4959",expr="Amp_OIII_5007/3")
+params.add("Amp_OIII_5007", value=70., min=0.001)
+params.add("Amp_OIII_4959", expr="Amp_OIII_5007/3")
 params.add("Amp_Hb",value=10., min=0.001)
-params.add("Amp_Ha",value=10., min=0.001)
-params.add("Amp_NII_1",value=5., min=0.001)
-params.add("Amp_NII_2",value=5., min=0.001)
+params.add("Amp_Ha",value=30., min=0.001)
+#params.add("Amp_NII_1",value=1., min=0.001, vary=False)
+#params.add("Amp_NII_2",value=1., min=0.001, vary=False)
 
 params.add("wave_OIII_5007", value=5035., min=5000., max=5080.)
 params.add("wave_OIII_4959", expr="wave_OIII_5007 - 47.9399")
 params.add("wave_Hb", expr="wave_OIII_5007 - 145.518 * (1+{0})".format(z))
 params.add("wave_Ha", expr="wave_OIII_5007 + 1556.375 * (1+{0})".format(z))
-params.add("wave_NII_1", expr="wave_OIII_5007 + 1541.621 * (1+{0})".format(z))
-params.add("wave_NII_2", expr="wave_OIII_5007 + 1577.031 * (1+{0})".format(z))
+#params.add("wave_NII_1", expr="wave_OIII_5007 + 1541.621 * (1+{0})".format(z))
+#params.add("wave_NII_2", expr="wave_OIII_5007 + 1577.031 * (1+{0})".format(z))
 
 params.add("FWHM", value=2.81, vary=False) # LSF
 params.add("Gauss_bkg", value=0.001)#, min=-500., max=500.)
@@ -88,16 +88,16 @@ list_of_std = np.array([np.abs(np.std(spec)) for spec in raw_data_list])
 input_errors = [np.repeat(list_of_std[i], len(wavelength)) for i in np.arange(0,len(list_of_std))]
 
 list_of_rN = np.zeros(len(raw_data_list))
-list_of_residuals = np.zeros((len(raw_data_list),len(wavelength)))
-list_of_residuals_from_fitter = np.zeros((len(raw_data_list),len(wavelength)))
-best_fit_A = np.zeros((len(raw_data_list),6))
-best_fit_mean = np.zeros((len(raw_data_list),6))
+#list_of_residuals = np.zeros((len(raw_data_list),len(wavelength)))
+#list_of_residuals_from_fitter = np.zeros((len(raw_data_list),len(wavelength)))
+best_fit_A = np.zeros((len(raw_data_list),4))
+#best_fit_mean = np.zeros((len(raw_data_list),6))
 
 for spec_n in raw_data_list_fitted:
     results = minimize(Gaussian_1D_res_full, params, args=(wavelength, raw_data_list[spec_n], input_errors[spec_n], spec_n), nan_policy="propagate")
-    best_fit_A[spec_n] = [results.params["Amp_OIII_5007"], results.params["Amp_OIII_4959"], results.params["Amp_Hb"], results.params["Amp_Ha"], results.params["Amp_NII_1"], results.params["Amp_NII_2"]]
-    best_fit_mean[spec_n] = [results.params["wave_OIII_5007"], results.params["wave_OIII_4959"], results.params["wave_Hb"], results.params["wave_Ha"], results.params["wave_NII_1"], results.params["wave_NII_2"]]
-    list_of_residuals_from_fitter[spec_n] = results.residual
+    best_fit_A[spec_n] = [results.params["Amp_OIII_5007"], results.params["Amp_OIII_4959"], results.params["Amp_Hb"], results.params["Amp_Ha"]]#, results.params["Amp_NII_1"], results.params["Amp_NII_2"]]
+    #best_fit_mean[spec_n] = [results.params["wave_OIII_5007"], results.params["wave_OIII_4959"], results.params["wave_Hb"], results.params["wave_Ha"], results.params["wave_NII_1"], results.params["wave_NII_2"]]
+    #list_of_residuals_from_fitter[spec_n] = results.residual
 
 
 print("fit complete")
@@ -106,13 +106,13 @@ gauss_A_OIII_5007 = [A[0] for A in best_fit_A]
 gauss_A_OIII_4959 = [A[1] for A in best_fit_A]
 gauss_A_Hb = np.array([A[2] for A in best_fit_A])
 gauss_A_Ha = [A[3] for A in best_fit_A]
-gauss_A_N_II_1 = [A[4] for A in best_fit_A]
-gauss_A_N_II_2 = [A[5] for A in best_fit_A]
+#gauss_A_N_II_1 = [A[4] for A in best_fit_A]
+#gauss_A_N_II_2 = [A[5] for A in best_fit_A]
 
-A_rN = np.array([A / rN for A,rN in zip(gauss_A_OIII_5007, list_of_rN)])
-A_rN_shape = A_rN.reshape(y_data,x_data)
+#A_rN = np.array([A / rN for A,rN in zip(gauss_A_OIII_5007, list_of_rN)])
+#A_rN_shape = A_rN.reshape(y_data,x_data)
 
-Gauss_F = np.array(gauss_A_OIII_5007) * np.sqrt(2*np.pi) * 1.19
-Gauss_F_shape = Gauss_F.reshape(y_data, x_data)
+#Gauss_F = np.array(gauss_A_OIII_5007) * np.sqrt(2*np.pi) * 1.19
+#Gauss_F_shape = Gauss_F.reshape(y_data, x_data)
 
 np.save("exported_data/FCC277/best_fit_A_multi", best_fit_A)
