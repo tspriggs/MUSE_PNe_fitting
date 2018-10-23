130da21f6ad530fb03b3b077639be9d89780306b
Thomas Spriggs <tspriggs@outlook.com>
2018-10-23 17:47:35 +0100

    add 1D check to galaxy_info, fix things, new plots

---
+++ pablo_reshape.py
@@ -0,0 +1,101 @@
+#####################################################
+# RECOVERING THE DIMESION OF THE ORIGINAL MUSE CUBE #
+#####################################################
+
+import numpy as np
+from astropy.io import fits
+
+import os
+
+import pdb
+
+cvel      = 299792.458
+
+def save_cube(data,header,name):
+    hdu = fits.PrimaryHDU()
+    hdu.data = np.copy(data)
+    hdu.header['CRVAL3']  = min(wave)
+    hdu.header['CRPIX3']  = 1.0
+    hdu.header['CDELT3'] = wave[1]-wave[0]
+
+    print('Data cube recovered in -->',name)
+
+#    if os.path.exists(name):
+#       os.remove(name)
+    hdu.writeto(name, overwrite=True)
+
+directory = 'FCC167_data/'
+#filename = 'FCC153_center.fits'
+filename = 'FCC167_DATACUBE_center.fits'
+#filename = 'FCC177_center.fits'
+galaxy   = filename[:6]
+
+vsys = {'FCC153':1638, 'FCC170': 1769., 'FCC177':1567, "FCC167":1841}
+
+hdu = fits.open(directory + filename)
+data  = hdu[1].data
+stat  = hdu[2].data
+hdr   = hdu[1].header
+s     = np.shape(data)
+spec  = np.reshape(data,[s[0],s[1]*s[2]])
+espec = np.reshape(stat,[s[0],s[1]*s[2]])
+
+# Getting the wavelength info
+wave = hdr['CRVAL3']+(np.arange(s[0])-hdr['CRPIX3'])*hdr['CD3_3']
+
+# Getting the spatial coordinates
+xaxis = np.arange(s[2])*hdr['CD2_2']*3600.0
+yaxis = np.arange(s[1])*hdr['CD2_2']*3600.0
+x, y  = np.meshgrid(xaxis,yaxis)
+x     = np.reshape(x,[s[1]*s[2]])
+y     = np.reshape(y,[s[1]*s[2]])
+pixelsize = x[1] - x[0]
+
+# Applying some wavelength cuts
+idx       = (wave >= 4700*(1.0 + vsys[galaxy]/cvel)) & \
+            (wave <= 6900*(1.0 + vsys[galaxy]/cvel))
+spec      = spec[idx,:]
+espec     = espec[idx,:]
+wave      = wave[idx]
+velscale  = (wave[1]-wave[0])*cvel/np.mean(wave)
+xy_extent = np.array( [data.shape[2], data.shape[1]] )
+
+idx_good = np.where( np.median(spec, axis=0) > 0.0 )[0]
+spec     = spec[:,idx_good]
+espec    = espec[:,idx_good]
+spec[np.isnan(spec)] = 0.0
+espec[np.isnan(espec)] = 0.0
+x        = x[idx_good]
+y        = y[idx_good]
+
+# Computing the SNR per spaxel
+signal = np.nanmedian(spec,axis=0)
+noise  = np.abs(np.nanmedian(np.sqrt(espec),axis=0))
+snr    = signal / noise
+
+# Open the residuals and the emission cube obtained with GandALF
+hdu1, hdu2      = fits.open(directory+galaxy+'_gandalf-residuals_SPAXEL.fits'), fits.open(directory+galaxy+'_gandalf-emission_SPAXEL.fits')
+data1, data2    = hdu1[1].data, hdu2[1].data
+resid, emission = data1['RESIDUALS'], data2['EMISSION'].T
+
+hdu  = fits.open(directory+galaxy+'_gandalf_SPAXEL.fits')
+data = hdu[2].data
+AoN  = data['AoN'][:,11]
+
+resid[np.isnan(resid)], emission[np.isnan(emission)] = 0.0, 0.0
+
+# Construct the residual cube and emission cube over the original coordinates
+free_points = s[1]*s[2]
+empty = np.zeros((len(spec),free_points))
+resid_tot    = np.copy(empty)
+emission_tot = np.copy(empty)
+AoN_tot      = np.zeros(free_points)
+resid_tot[:,idx_good] = np.copy(resid.T)
+emission_tot[:,idx_good] = np.copy(emission)
+AoN_tot[idx_good] = np.copy(AoN)
+#pdb.set_trace()
+
+# Save the data cubes
+save_cube(resid_tot.reshape((len(spec),s[1],s[2])),hdr,galaxy+'_residuals.fits')
+save_cube(emission_tot.reshape((len(spec),s[1],s[2])),hdr,galaxy+'_emission.fits')
+save_cube(AoN_tot.reshape((s[1],s[2])),hdr,galaxy+'_AoN.fits')
--- reshape_galaxy.py
+++ reshape_galaxy.py
@@ -29,26 +29,26 @@ index_pix = np.zeros((len(x_pix),2))
 for n,(i,j) in enumerate(zip(x_pix, y_pix)):
     index_pix[n] = [np.squeeze(np.where(xaxis == i)), np.squeeze(np.where(yaxis==j))]
 
-clean_hdulist = fits.open(galaxy_data["clean cube"])
+clean_hdulist = fits.open(galaxy_data["Galaxy name"] + "_data/" + galaxy_data["Galaxy name"] +"_gandalf-cleaned_SPAXEL.fits")
 wavelength = np.exp(clean_hdulist[2].data.LOGLAM)
 
 # read in the residual cube from gandalf and order the data as (list of spectra, wavelength range length)
 res_hdu = fits.open(galaxy_data["Galaxy name"] + "_data/" + galaxy_data["Galaxy name"] + "_gandalf-residuals_SPAXEL.fits")
 res_data_list = res_hdu[1].data.RESIDUALS
-res_data_list = np.swapaxes(res_data_list, 1, 0)
+#res_data_list = np.swapaxes(res_data_list, 1, 0)
 
 # Make an empty pointing of the same shape as the input raw data cube: y, x, lambda
-empty_gal = np.zeros((raw_shape[1], raw_shape[2], len(wavelength)))
+empty_gal = np.zeros((len(wavelength), raw_shape[1], raw_shape[2], ))
 
 # Fill in the empty pointing, using the x,y index values of the fitted pixel locations.
 for n, i in enumerate(index_pix):
-    empty_gal[int(i[1])][int(i[0])] = res_data_list[n]
+    empty_gal[:,int(i[1]),int(i[0])] = res_data_list[:,n]
 
-reshaped_gal = np.transpose(empty_gal, (2,0,1)) 
+#reshaped_gal = np.transpose(empty_gal, (2,0,1)) 
 
 # Save galaxy
 write_hdu = fits.PrimaryHDU()
-write_hdu.data = reshaped_gal
+write_hdu.data = empty_gal
 write_hdu.header['CRVAL3'] = min(wavelength)
 write_hdu.header['CRPIX3'] = 1.0
 write_hdu.header['CDELT3'] = wavelength[1]-wavelength[0]
